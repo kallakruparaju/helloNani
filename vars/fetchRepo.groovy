68dcda79667936653fec6ccfefffb3efaa9bb77c
Akash Kola <63763012+Akashnani-star@users.noreply.github.com>
2022-09-21 21:11:53 +0530

    Update fetchRepo.groovy

--- vars/fetchRepo.groovy
+++ vars/fetchRepo.groovy
@@ -1,10 +1,24 @@
-def call(){
-    pipeline{
-        agent any
-        stages{
-            stage("HelloNani"){
-                steps{
-                    sh "echo hello"
+def call(num){
+    if(num == 10){
+        pipeline{
+            agent any
+            stages{
+                stage("HelloNani"){
+                    steps{
+                        sh "echo num is $num"
+                    }
+                }
+            }
+        }
+    }
+    else{
+        pipeline{
+            agent any
+            stages{
+                stage("Hey"){
+                    steps{
+                        sh "echo num is not 10"
+                    }
                 }
             }
         }
