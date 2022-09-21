369dbb4ef8d1da61783fb8e68e4d3b9bd2bec7a0
Akash Kola <63763012+Akashnani-star@users.noreply.github.com>
2022-09-21 16:29:37 +0530

    Update fetchRepo.groovy

--- vars/fetchRepo.groovy
+++ vars/fetchRepo.groovy
@@ -2,4 +2,14 @@ def call(branchname,reponame){
     print branchname
     println reponame
     git branch: "${branchname}", url: "${reponame}"
+    pipeline{
+        agent any
+        stages{
+            stage{
+                steps{
+                    sh "echo hello"
+                }
+            }
+        }
+    }
 }
